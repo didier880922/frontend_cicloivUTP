736f99abb022e9a26366efe618022c293c3a48a6
Luis Manuel Palmera Quintero <lmpalmera04@gmail.com>
2022-10-19 21:59:51 -0500

    semana2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proyecto con React - UTP MISIÓN TIC GRUPOS 10, 16, 14</h1>
+    </div>
+  );
+}
+
+export default App;
