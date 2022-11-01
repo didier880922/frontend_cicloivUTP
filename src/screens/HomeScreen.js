3e63b860c5387726512513be1987afedd41502a0
Luis Manuel Palmera Quintero <dfguerrero@unicesar.edu.co>
2022-11-01 17:04:13 -0500

    martes
    martes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,52 @@
-import { Link } from 'react-router-dom'
-import data from '../data.js';
+import { useEffect, useReducer } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+//import data from "../";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true};
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false};
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload};
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
+  const [{loading, error, products}, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true, error: '',
+  });
+
+  //const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fecthData = async () => {
+      dispatch({type: 'FETCH_REQUEST'});
+      try {
+        const result = await axios.get('http://localhost:30000/api/products')
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({type: 'FETCH_FAIL', payload: err.message});
+      }
+    };
+    fecthData();
+  }, []);
   return (
     <div>
       <h1>Lista de productos</h1>
       <div className="products">
-        {data.products.map((product) => (
-          <div className="product">
+        {loading ? (
+             <div>Cargando datos...</div>
+          ) : error ? ( 
+          <div>{error}</div>
+          ) : (
+        products.map((product) => (
+          <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </Link>
@@ -16,12 +55,14 @@ function HomeScreen() {
                 <p>{product.name}</p>
               </a>
               <p>{product.price}</p>
+              <button>Agregar al carrito</button>
             </div>
           </div>
-        ))}
+        ))
+      )}
       </div>
     </div>
   );
-};
+}
 
 export default HomeScreen;
