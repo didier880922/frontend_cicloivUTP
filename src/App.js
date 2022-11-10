486d906f9f7d62b5cba711917ae26cd0c5e10497
Luis Manuel Palmera Quintero <dfguerrero@unicesar.edu.co>
2022-11-09 19:20:12 -0500

    MIERCOLES
    Miercoles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomeScreen  from './screens/HomeScreen';
-import { Link } from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Badge from "react-bootstrap/Badge";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import { useContext } from 'react';
+import { Store } from "./Store";
 
 function App() {
+
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
-    <div className="App">
-      <header>
-        <Link to="/">Tienda virtual</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} />
-        </Routes>
-      </main>
-    </div>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Vende Digital</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">Todos los derechos reservados @2022</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
